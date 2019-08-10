257c7a1e9ee96d2cd011a7b71c3450352e3a9ca1
Kenney Knisely <kknisely@mac.com>
2019-08-09 21:46:45 -0400

    Transpile ES6 using Babel

---
+++ src/stack.js
@@ -0,0 +1,312 @@
+let round = 0;
+
+let answerTime = 0;
+
+const cards = document.getElementsByClassName("card");
+
+const card1 = document.getElementById("one");
+const card2 = document.getElementById("two");
+const card3 = document.getElementById("three");
+const card4 = document.getElementById("four");
+
+const seconds = 500;
+
+const faceCard = [
+  //0
+  {"image":"img/magician.png",
+  "name":"magician"
+  },
+
+  //1
+  {"image":"img/popess.png",
+  "name":"popess"
+  },
+
+  //2
+  {"image":"img/empress.png",
+  "name":"empress"
+  },
+
+  //3
+  {"image":"img/emperor.png",
+  "name":"emperor"
+  },
+
+  //4
+  {"image":"img/pope.png",
+  "name":"pope"
+  },
+
+  //5
+  {"image":"img/lovers.png",
+  "name":"lovers"
+  },
+
+  //6
+  {"image":"img/chariot.png",
+  "name":"chariot"
+  },
+
+  //7
+  {"image":"img/justice.png",
+  "name":"justice"
+  },
+
+  //8
+  {"image":"img/hermit.png",
+  "name":"hermit"
+  },
+
+  //9
+  {"image":"img/fortune.png",
+  "name":"wheeloffortune"
+  },
+
+  //10
+  {"image":"img/strength.png",
+  "name":"strength"
+  },
+
+  //11
+  {"image":"img/hanged.png",
+  "name":"hangedman"
+  },
+
+  //12
+  {"image":"img/death.png",
+  "name":"death"
+  },
+
+  //13
+  {"image":"img/temperance.png",
+  "name":"temperance"
+  },
+
+  //14
+  {"image":"img/devil.png",
+  "name":"devil"
+  },
+
+  //15
+  {"image":"img/tower.png",
+  "name":"star"
+  },
+
+  //16
+  {"image":"img/moon.png",
+  "name":"moon"
+  },
+
+  //17
+  {"image":"img/sun.png",
+  "name":"sun"
+  },
+
+  //18
+  {"image":"img/judgement.png",
+  "name":"judgement"
+  },
+
+  //19
+  {"image":"img/world.png",
+  "name":"world"
+  },
+
+  //20
+  {"image":"img/fool.png",
+  "name":"fool"
+  },
+];
+
+const numberCard = [
+  //0
+  {"image": "img/2.png",
+  "number": 2},
+
+  //1
+  {"image": "img/3.png",
+  "number": 3},
+
+  //2
+  {"image": "img/4.png",
+  "number": 4},
+
+  //3
+  {"image": "img/5.png",
+  "number": 5},
+
+  //4
+  {"image": "img/6.png",
+  "number": 6},
+
+  //5
+  {"image": "img/7.png",
+  "number": 7},
+
+  //6
+  {"image": "img/8.png",
+  "number": 8},
+
+  //7
+  {"image": "img/9.png",
+  "number": 9},
+
+  //8
+  {"image": "img/10.png",
+  "number": 10},
+];
+
+const fortunes = [
+  "A beautiful, smart, and loving person will be coming into your life.",
+  "From now on your kindness will lead you to success.",
+  "In the end all things will be known.",
+  "A lifetime friend shall soon be made",
+  "Physical activity will dramatically improve your outlook today.",
+  "Savor your freedom – it is precious.",
+  "The smart thing to do is to begin trusting your intuitions.",
+  "Your moods signal a period of change.",
+  "Many will travel to hear you speak.",
+  "Never fear! The end of something marks the start of something new.",
+];
+
+function open (){
+  faceCard.sort(function(a, b){return 0.5 - Math.random()});
+
+  card1.src = faceCard[0].image;
+  card2.src = faceCard[1].image;
+  card3.src = faceCard[2].image;
+  card4.src = faceCard[3].image;
+}
+
+function cardChange (){
+  if( round === 1){
+    numberCard.sort(function(a, b){return 0.5 - Math.random()});
+
+    card1.src = numberCard[0].image;
+    card2.src = numberCard[1].image;
+    card3.src = numberCard[2].image;
+    card4.src = numberCard[3].image;
+  } else if( round === 2){
+    faceCard.sort(function(a, b){return 0.5 - Math.random()});
+
+    card1.src = faceCard[0].image;
+    card2.src = faceCard[1].image;
+    card3.src = faceCard[2].image;
+    card4.src = faceCard[3].image;
+  };
+};
+
+const toggle = () => document.getElementById("stars").classList.toggle("on");
+
+function selectCard(){
+
+  card1.addEventListener("click",function(){
+    if(round===1){
+      for (let i = 0; i < numberCard[0].number * 2; i++){
+        setTimeout(toggle, i*seconds);
+      };
+    } else {
+        answerTime = faceCard[0].name.length
+        for (let i = 0; i < faceCard[0].name.length * 2; i++){
+          setTimeout(toggle, i*seconds);
+        };
+    }
+  });
+
+  card2.addEventListener("click",function(){
+    if(round===1){
+      for (let i = 0; i < numberCard[1].number * 2; i++){
+        setTimeout(toggle, i*seconds);
+      };
+    } else {
+        answerTime = faceCard[1].name.length
+        for (let i = 0; i < faceCard[1].name.length * 2; i++){
+          setTimeout(toggle, i*seconds);
+        };
+    }
+  });
+
+  card3.addEventListener("click",function(){
+    if(round===1){
+      for (let i = 0; i < numberCard[2].number * 2; i++){
+        setTimeout(toggle, i*seconds);
+      };
+    } else {
+        answerTime = faceCard[2].name.length
+        for (let i = 0; i < faceCard[2].name.length * 2; i++){
+          setTimeout(toggle, i*seconds);
+        };    }
+  });
+
+  card4.addEventListener("click",function(){
+    if(round===1){
+      for (let i = 0; i < numberCard[3].number * 2; i++){
+        setTimeout(toggle, i*seconds);
+    };
+  } else {
+      answerTime = faceCard[3].name.length
+      for (let i = 0; i < faceCard[3].name.length * 2; i++){
+        setTimeout(toggle, i*seconds);
+      };    }
+  });
+
+
+};
+
+function answer (){
+  if(round === 3){
+    fortunes.sort(function(a, b){return 0.5 - Math.random()});
+
+    document.getElementById("fortune-text").innerHTML = fortunes[0];
+  }
+};
+
+function hideText (){
+  document.getElementById("instructions").style.display = "none";
+}
+
+
+/* Stacking card code*/
+
+
+$('.stack').click(function() {
+
+    round++;
+    setTimeout(cardChange, 2000);
+    setTimeout(answer, answerTime * 1000);
+    console.log(answerTime);
+
+
+  $(".card").each(function(e) {
+
+    setTimeout(function() {
+      $(".card").eq(e).attr("class", "card");
+    }, e * 150)
+
+  });
+
+});
+
+
+/* Spreading card code */
+
+$('.spread').click(function() {
+
+  hideText();
+
+  $(".card").each(function(e) {
+
+    setTimeout(function() {
+      $(".card").eq(e).attr("class", "card ani" + e);
+    }, e * 150)
+
+  });
+
+
+});
+
+
+window.onload = function(){
+  open();
+};
+selectCard();
